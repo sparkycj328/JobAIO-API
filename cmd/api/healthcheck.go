c54a5186e92d022b678aad83fcd98a3f95fe9175
sparkycj328 <sparkycj328@gmail.com>
2023-02-17 01:30:11 -0600

    Changed manual JSON object into map

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,22 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // healthcheckHandler writes plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	// marshal the json object into bytes
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request",
+			http.StatusInternalServerError)
+		return
+	}
 
-	// create a JSON-object to return to the response writer
-	js := `{"status":"available", "environment":%q, "version":%q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// append a newline to make the JSON more viewer-friendly
+	js = append(js, '\n')
 
 	// sets a response header so client knows response contains json
 	w.Header().Set("Content-Type", "application/json")
 
 	// write the JSON as the HTTP response body
-	w.Write([]byte(js))
-
+	w.Write(js)
 }
