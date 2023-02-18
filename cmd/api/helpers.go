d658b7fe8bbec70b2364981c1c0d7f6d5e800aa1
sparkycj328 <sparkycj328@gmail.com>
2023-02-17 23:23:23 -0600

    Move writeJSON function to appropriate file

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
@@ -12,6 +13,7 @@ func alphaNumeric(name string) (string, bool) {
 	return name, regexp.MustCompile(`^[a-zA-Z0-9]*$`).MatchString(name)
 }
 
+// readNameParam will grab the parameter from the URL using the request context
 func (app *application) readNameParam(r *http.Request) (string, error) {
 	// retrieve a slice containing any interpolated parameter names and values
 	params := httprouter.ParamsFromContext(r.Context())
@@ -23,4 +25,31 @@ func (app *application) readNameParam(r *http.Request) (string, error) {
 	}
 
 	return name, nil
+}
+
+// writeJSON is a helper function which will iterate through the data passed and convert it into
+// a JSON object to return.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// append a newline to make the JSON more viewer-friendly
+	js = append(js, '\n')
+
+	// iterate through map and write the Header for each key-value pair
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// sets a response header so client knows response contains json
+	w.Header().Set("Content-Type", "application/json")
+
+	// write the status code for the response
+	w.WriteHeader(status)
+	// write the JSON as the HTTP response body
+	w.Write(js)
+
+	return nil
 }
