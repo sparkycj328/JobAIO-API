9dac6ea174aadb79aba9e8bcfbb1e7ae14b99fd2
Carlos <sparkycj328@gmail.com>
2023-02-22 11:43:09 -0600

    Refactor countries struct into inline declaration

--- internal/data/companies.go
+++ internal/data/companies.go
@@ -6,16 +6,14 @@ import (
 )
 
 type Company struct {
-	Name      string    `json:"company"`   // company name
-	Countries []Country `json:"countries"` // job postings
-}
-
-type Country struct {
-	ID        int64     `json:"id"`      // Unique integer id for the company
-	Country   string    `json:"country"` // Country name
-	Total     int       `json:"total"`   // total amount of job available
-	URL       string    `json:"url"`     // URL location where resource is located
-	CreatedAt time.Time `json"-"`        // created timestamp for the data
+	Name      string `json:"company"` // company name
+	Countries []struct {
+		ID        int64     `json:"id"`      // Unique integer id for the company
+		Country   string    `json:"country"` // Country name
+		Total     int       `json:"total"`   // total amount of job available
+		URL       string    `json:"url"`     // URL location where resource is located
+		CreatedAt time.Time `json:"created"` // created timestamp for the data
+	} `json:"countries"` // job postings
 }
 
 // ValidateCompany will perform validation checks on each
