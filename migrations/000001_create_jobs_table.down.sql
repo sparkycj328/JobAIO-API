666f10371a72c47f2fa2c5512e0f37b8c1269906
Carlos <sparkycj328@gmail.com>
2023-02-25 21:14:24 -0600

    Include SQL migrations for recreation purposes

---
+++ migrations/000001_create_jobs_table.down.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS movies (
+    id bigserial PRIMARY KEY,
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    vendor text NOT NULL,
+    country text NOT NULL,
+    amount integer NOT NULL,
+    url text NOT NULL,
+    );
