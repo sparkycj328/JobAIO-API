de9d2c413a15a29e71bf7df459f4f0beac4ac513
Carlos <sparkycj328@gmail.com>
2023-02-25 22:01:55 -0600

    Set initial command for postgresql down migration

--- migrations/000001_create_jobs_table.down.sql
+++ migrations/000001_create_jobs_table.down.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS movies (
+CREATE TABLE IF NOT EXISTS jobs (
     id bigserial PRIMARY KEY,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     vendor text NOT NULL,
